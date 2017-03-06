b03f0216d3f3338731799f9d525eabcebf4036f8
andrey.tarashevskiy <fantocci@gmail.com>
2017-03-06 11:27:41 +0300

    Changed dependency from java.util.Base64 to org.apache.commons.codec.binary.Base64 to support jre < 1.8

--- src/main/java/net/avalara/avatax/rest/client/AvaTaxClient.java
+++ src/main/java/net/avalara/avatax/rest/client/AvaTaxClient.java
@@ -1,17 +1,16 @@
 package net.avalara.avatax.rest.client;
 
 import com.google.gson.reflect.TypeToken;
-import net.avalara.avatax.rest.client.models.*;
 import net.avalara.avatax.rest.client.enums.*;
+import net.avalara.avatax.rest.client.models.*;
+import org.apache.commons.codec.binary.Base64;
 
 import java.math.BigDecimal;
-import java.util.Base64;
+import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import java.util.ArrayList;
 
 public class AvaTaxClient {
 
@@ -59,7 +58,7 @@ public class AvaTaxClient {
         String header = null;
 
         try {
-            header = Base64.getEncoder().encodeToString((username + ":" + password).getBytes("utf-8"));
+            header = Base64.encodeBase64String((username + ":" + password).getBytes("utf-8"));
         } catch (java.io.UnsupportedEncodingException ex) {
             System.out.println("Could not find encoding for UTF-8.");
             ex.printStackTrace();
