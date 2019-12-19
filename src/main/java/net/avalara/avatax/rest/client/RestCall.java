409b18002b38d5d80878bb027e6905461d6bddce
Avalara-ChrisWalker <Chris.Walker@Avalara.com>
2019-12-19 10:09:38 -0800

    Fixed19.12 update

--- src/main/java/net/avalara/avatax/rest/client/RestCall.java
+++ src/main/java/net/avalara/avatax/rest/client/RestCall.java
@@ -110,7 +110,7 @@ public class RestCall<T> implements Callable<T> {
     }
 
     private void buildRequest(HttpRequestBase baseRequest) {
-        String clientId = String.format("%s; %s; %s; %s; %s", appName, appVersion, "JavaRestClient", "19.11.0", machineName);
+        String clientId = String.format("%s; %s; %s; %s; %s", appName, appVersion, "JavaRestClient", "19.12.0", machineName);
         baseRequest.setHeader(AvaTaxConstants.XClientHeader, clientId);
     }
 }
