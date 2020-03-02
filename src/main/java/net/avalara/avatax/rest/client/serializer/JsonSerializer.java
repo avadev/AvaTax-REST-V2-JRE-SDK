3580055cb76c0bb4d862dba27eb61f08b82594aa
Chris.Walker <chris.walker@avalara.com>
2020-03-02 14:11:01 -0800

    fixed error

--- src/main/java/net/avalara/avatax/rest/client/serializer/JsonSerializer.java
+++ src/main/java/net/avalara/avatax/rest/client/serializer/JsonSerializer.java
@@ -15,21 +15,37 @@ import java.text.SimpleDateFormat;
 public class JsonSerializer<T> {
     private static ObjectMapper objectMapper = new ObjectMapper().configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true);
 
-    public static String SerializeObject(Object obj) throws JsonProcessingException{
-        DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
-        objectMapper.setDateFormat(df);
-       return objectMapper.writeValueAsString(obj);
+    public static String SerializeObject(Object obj){
+        String str = null;
+        try {
+            str = objectMapper.writeValueAsString(obj);
+        } catch (JsonProcessingException e){}
+        return str;
+
     }
 
-    public static Object DeserializeObject(String json, TypeReference type) throws JsonProcessingException {
-        DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
-        objectMapper.setDateFormat(df);
-        return objectMapper.readValue(json, type);
+    public static Object DeserializeObject(String json, TypeReference type){
+        Object obj = null;
+
+        try {
+            DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+            objectMapper.setDateFormat(df);
+            obj = objectMapper.readValue(json, type);
+        } catch (JsonProcessingException e){}
+
+        return obj;
+
     }
 
-    public static Object DeserializeObject(String json, Class type) throws JsonProcessingException {
-        DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
-        objectMapper.setDateFormat(df);
-        return objectMapper.readValue(json, type);
+    public static Object DeserializeObject(String json, Class type){
+        Object obj = null;
+
+        try {
+            DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+            objectMapper.setDateFormat(df);
+            obj = objectMapper.readValue(json, type);
+        } catch (JsonProcessingException e){}
+
+        return obj;
     }
 }
